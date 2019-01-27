b3fa630bcb3e3a42c798995caf79bf8ea5cb68f8
Vince14Genius <vince14genius@icloud.com>
2019-01-27 17:32:44 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,15 @@
+function beginLoadJSON(filePath, shouldUseDefinitionAsFront) {
+    loadJSON(filePath, didLoadJSON)
+    shouldUseDefinitionAsFront = shouldUseDefinitionAsFront
+}
 
+function didLoadJSON(loadedJSON) {
+    name = loadedJSON.name
+    congratulateCard = loadedJSON.congratulateCard
+    flashcards = loadedJSON.flashcards
+}
+
+// You can modify the following code or move them elsewhere
+beginLoadJSON("demo-data.json", shouldUseDefinitionAsFront)
+//termFontSizeMultiplier = 1/7
+//definitionFontSizeMultiplier = 1/12
