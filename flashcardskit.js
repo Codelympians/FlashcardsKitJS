041723d538a5b679c4cb7e9566db59bb267e4021
Vince14Genius <vince14genius@icloud.com>
2019-01-27 17:05:32 +0800

    Update flashcardskit.js

--- flashcardskit.js
+++ flashcardskit.js
@@ -1,5 +1,6 @@
 /* Customizable Variables */
 
+let name = "FlashcardsKitJS Demo"
 let termFontSizeMultiplier = 1/7
 let definitionFontSizeMultiplier = 1/12
 let flashcards = []
@@ -13,38 +14,6 @@ let shouldUseDefinitionAsFront = false
 
 const instructionsFontSizeMultiplier = 1/18
 const border = 100
-
-let fpsEngine = {
-    oldTime: 0, // Time at which the previous frame is updated
-    fpsCooldown: 0, // When fpsCooldown <= 0, set displayFPS = fps
-    displayFPS: 0, // fps displayed on screen, updated every second
-    realFPS: 0,
-    
-    // Functions related to FPS
-    // Usage: insert them at the end of the draw() function
-    updateFPS: function() {
-        let currentTime = millis()
-        let fpsDelta = currentTime - this.oldTime
-
-        // Update realFPS
-        this.realFPS = frameRate() // function provided by p5.js API
-        
-        // Update displayFPS
-        this.fpsCooldown -= fpsDelta
-        if (this.fpsCooldown <= 0) {
-            this.displayFPS = this.realFPS
-            this.fpsCooldown = 1000
-        }
-
-        this.oldTime = currentTime
-    },
-    showFPS: function(r, g, b) {
-        fill(r, g, b)
-        textAlign(RIGHT, TOP) // Text alignment of the fps label
-        textSize(24)
-        text(`${Math.floor(this.displayFPS)} fps`, width - 16, 16) // Position of the fps label
-    }
-}
 
 let cardWidth = null
 let cardHeight = null
@@ -159,6 +128,13 @@ function displayCredits() {
     text("Powered by github.com/Codelympians/FlashcardsKitJS", windowWidth - 12, windowHeight - 12)
 }
     
+function displayName() {
+    fill(160)
+    textAlign(RIGHT, TOP) // Text alignment of the fps label
+    textSize(24)
+    text(name, width - 16, 16) // Position of the fps label
+}
+
 function mousePressed() {
     if (isShowingAnswer) {
         cardSelect()
@@ -175,15 +151,11 @@ function setup() {
 
 function draw() {
     // draw() runs every time before a new frame is rendered. 
-
     background(20)
     displayFlashcard()
     displayProgress()
     displayCredits()
-  
-    // Update and show FPS
-    fpsEngine.updateFPS()
-    fpsEngine.showFPS(100, 100, 100)
+    displayName()
 }
 
 function windowResized() {
