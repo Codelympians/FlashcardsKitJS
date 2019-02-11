5a08b0dc06ab68b23c6647ffc21c4e2826bf86d4
Vince14Genius <vince14genius@icloud.com>
2019-02-11 10:58:30 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,29 +1,7 @@
-'use strict'
-
-let didAttemptToLoadJSON = false
-
-function tryLoadingJSON(filePath, shouldUseDefinitionAsFront) {
-    if (!didAttemptToLoadJSON) {
-        loadJSON(filePath, didLoadJSON)
-        shouldUseDefinitionAsFront = shouldUseDefinitionAsFront
-        didAttemptToLoadJSON = true
-        console.log("Loading Process: willLoad")
-    }
-}
-
-function didLoadJSON(loadedJSON) {
-    name = loadedJSON.name
-    congratulateCard = loadedJSON.congratulateCard
-    flashcards = loadedJSON.flashcards
-    deck = flashcards.slice()
-    console.log("Loading Process: didLoad")
-}
-
-// You can modify the following code or move them elsewhere
+// You can modify the following code
 function userDefinedLoading() {
     termFontSizeMultiplier = 1/8
     definitionFontSizeMultiplier = 1/14
     tryLoadingJSON("demo-data.json", false)
     console.log("Loading Process: didAttemptToLoad")
 }
-
